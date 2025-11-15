18fd4a479deec61862b5fcced409e6aae5b292fc
mazikaru492 <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2025-11-15 14:43:59 +0900

    ページのスタイルと画像コンポーネントを更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
-  const name ="世界";
   return (
     <section className={styles.top}>
       <div>
-        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-        <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
       </div>
-    <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
     </section>
   );
 }
