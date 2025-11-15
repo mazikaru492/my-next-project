2d99101a553bba823d31299649e14241470be714
mazikaru492 <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2025-11-15 13:32:59 +0900

    jhh

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
         <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
       </div>
-    <img className={styles.image} src="/img-mv.jpg" alt="" />
+    <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
     </section>
   );
 }
