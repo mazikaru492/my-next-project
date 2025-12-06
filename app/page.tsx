47532eb4aef3073d1b0b03ede4b244d91b16de69
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2025-12-06 15:14:51 +0900

    feat: ニュースリストコンポーネントをインポートし、表示ロジックを整理

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Image from "next/image";
+import NewsList from "@/app/components/ButtonLink/NewsList";
 import ButtonLink from "@/app/components/ButtonLink";
 import { News } from "@/app/lids/microcms";
 
@@ -59,31 +60,7 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <ul>
-          {sliceData.map((article) => (
-            <li key={article.id} className={styles.newsItem}>
-              <div className={styles.link}>
-                <Image
-                  className={styles.image}
-                  src="/no-image.png"
-                  alt="No Image"
-                  width={1200}
-                  height={630}
-                />
-                <dl className={styles.contents}>
-                  <dt className={styles.newsItemTitle}>{article.title}</dt>
-                  <dd className={styles.meta}>
-                    <span className={styles.tag}>{article.category.name}</span>
-                    <span className={styles.date}>
-                      <Image src="/clock.svg" alt="" width={16} height={16} />
-                      {article.publishedAt}
-                    </span>
-                  </dd>
-                </dl>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <div className={styles.newsList} />
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっと見る</ButtonLink>
         </div>
