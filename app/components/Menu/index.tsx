731cfce80ae507ee8ecb3570ceab089850d8b9d5
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2025-12-07 13:26:35 +0900

    feat: メニューコンポーネントにボタンを追加し、スタイルを整備

--- app/components/Menu/index.tsx
+++ app/components/Menu/index.tsx
@@ -4,18 +4,23 @@ import style from "./index.module.css";
 
 export default function Menu() {
   return (
-    <nav className={style.nav}>
-      <ul className={style.items}>
-        <li>
-          <Link href="/news">ニュース</Link>
-        </li>
-        <li>
-          <Link href="/members">メンバー</Link>
-        </li>
-        <li>
-          <Link href="/contact">お問い合わせ</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav className={style.nav}>
+        <ul className={style.items}>
+          <li>
+            <Link href="/news">ニュース</Link>
+          </li>
+          <li>
+            <Link href="/members">メンバー</Link>
+          </li>
+          <li>
+            <Link href="/contact">お問い合わせ</Link>
+          </li>
+        </ul>
+      </nav>
+      <button className={style.button}>
+        <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
+      </button>
+    </div>
   );
 }
