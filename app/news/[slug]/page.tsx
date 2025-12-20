0d367ddb9b96f1278d50d4449de0c85f56830902
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2025-12-20 14:31:42 +0900

    7章終了

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -8,9 +8,12 @@ type Props = {
   params: {
     slug: string;
   };
+  searchParams: {
+    dk?: string;
+  };
 };
-export default async function Page({ params }: Props) {
-  const data = await getNewsDetail(params.slug);
+export default async function Page({ params, searchParams }: Props) {
+  const data = await getNewsDetail(params.slug, { draftKey: searchParams.dk });
 
   if (!data) {
     notFound();
